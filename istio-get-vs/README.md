03a06a6020f5f3089e3259b2ddc60a12d5bf82b3
yokawasa <yokawasa@gmail.com>
2021-05-03 07:30:07 +0900

    updated istio-get-vs

--- istio-get-vs/README.md
+++ istio-get-vs/README.md
@@ -3,10 +3,15 @@
 
 Get a VirtualService named `your-name` in namespace of `your-namespace`
 ```
-istio-get-vs -name <your-name> -namespace <your-namespace>
+istio-get-vs -s <your-name> -n <your-namespace>
 ```
 
 List VirtualServices  in namespace of `your-namespace`
 ```
-istio-get-vs -namespace <your-namespace>
+istio-get-vs -n <your-namespace>
 ```
+
+# References
+- https://istio.io/latest/blog/2019/announcing-istio-client-go/
+- https://github.com/istio/client-go/blob/release-1.9/cmd/example/client.go
+- https://pkg.go.dev/istio.io/client-go/pkg/clientset/versioned
--- istio-get-vs/main.go
+++ istio-get-vs/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"log"
 	"os"
 
@@ -11,66 +12,84 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-// https://istio.io/latest/blog/2019/announcing-istio-client-go/
-// https://github.com/istio/client-go/blob/release-1.9/cmd/example/client.go
-// api references
-// https://pkg.go.dev/istio.io/client-go/pkg/clientset/versioned
+var KubeConfig = flag.String("kubeconfig", "", "kubeconfig file")
 
-var (
-	KubeConfig = flag.String("kubeconfig", "", "kubeconfig file")
-)
+func usage() {
+	fmt.Println(usageText)
+	os.Exit(1)
+}
+
+var usageText = `istio-get-vs [options...]
+
+Options:
+-n string  Namespace By default "default"
+-s string  VirtualService name
+-h         help message
+`
 
 func main() {
 
 	var (
 		namespace string
-		name      string
+		vsname    string
 	)
-	flag.StringVar(&namespace, "namespace", "default", "namespace")
-	flag.StringVar(&name, "name", "", "virtual service name")
+	flag.StringVar(&namespace, "n", "default", "namespace")
+	flag.StringVar(&vsname, "s", "", "VirtualService name")
+	flag.Usage = usage
 	flag.Parse()
 
-	// create kubernetes client
-	client, err := newClient(*KubeConfig)
+	client, err := newIstioClient(*KubeConfig)
 	if err != nil {
-		log.Fatalf("Failed to create istio client: %s", err)
+		fmt.Printf("[ERROR] Failed to create istio client: %s\n", err)
+		os.Exit(1)
 	}
 
-	if name != "" {
-		log.Printf("Get VirtualService: %v (namespace: %v)\n", name, namespace)
+	if vsname != "" {
+		log.Printf("Get VirtualService: %v (namespace: %v)\n", vsname, namespace)
 		// Get VirtualService
-		vs, err := client.NetworkingV1alpha3().VirtualServices(namespace).Get(context.TODO(), name, v1.GetOptions{})
+		vs, err := client.NetworkingV1alpha3().VirtualServices(namespace).Get(
+			context.TODO(),
+			vsname,
+			v1.GetOptions{})
 		if err != nil {
-			log.Fatalf("Failed to get VirtualService %s in %s namespace: %s", name, namespace, err)
+			fmt.Printf("[ERROR] Failed to get VirtualService %s in %s namespace: %s\n", vsname, namespace, err)
+			os.Exit(1)
 		}
-		log.Printf("VirtualService Hosts %+v Gateway %+v Http %+v\n", vs.Spec.Hosts, vs.Spec.Gateways, vs.Spec.Http)
+		fmt.Printf("VirtualService Hosts %+v Gateway %+v Http %+v\n",
+			vs.Spec.Hosts,
+			vs.Spec.Gateways,
+			vs.Spec.Http)
 
 	} else {
-		log.Printf("List VirtualServiceis (namespace: %v)\n", namespace)
-		// Print all VirtualServices
-		vsList, err := client.NetworkingV1alpha3().VirtualServices(namespace).List(context.TODO(), v1.ListOptions{})
+		fmt.Printf("List VirtualServiceis (namespace: %v)\n", namespace)
+		// Print all VirtualServices in the namespace
+		vsList, err := client.NetworkingV1alpha3().VirtualServices(namespace).List(
+			context.TODO(),
+			v1.ListOptions{})
 		if err != nil {
-			log.Fatalf("Failed to get VirtualService in %s namespace: %s", namespace, err)
+			fmt.Printf("[ERROR] Failed to get VirtualService in %s namespace: %s\n", namespace, err)
+			os.Exit(1)
 		}
 		for i := range vsList.Items {
 			vs := vsList.Items[i]
-			log.Printf("VirtualService Hosts %+v Gateway %+v Http %+v\n", vs.Spec.Hosts, vs.Spec.Gateways, vs.Spec.Http)
+			fmt.Printf("VirtualService Hosts %+v Gateway %+v Http %+v\n",
+				vs.Spec.Hosts,
+				vs.Spec.Gateways,
+				vs.Spec.Http)
 		}
 	}
 }
 
-func newClient(kubeConfigPath string) (versionedclient.Interface, error) {
+func newIstioClient(kubeConfigPath string) (versionedclient.Interface, error) {
 	if kubeConfigPath == "" {
 		kubeConfigPath = os.Getenv("KUBECONFIG")
 	}
 	if kubeConfigPath == "" {
-		// use default path(.kube/config) if kubeconfig path is not set
-		kubeConfigPath = clientcmd.RecommendedHomeFile
+		kubeConfigPath = clientcmd.RecommendedHomeFile // use default path(.kube/config)
 	}
 	kubeConfig, err := clientcmd.BuildConfigFromFlags("", kubeConfigPath)
 	if err != nil {
 		return nil, err
 	}
-	//return kubernetes.NewForConfig(kubeConfig)
 	return versionedclient.NewForConfig(kubeConfig)
 }
